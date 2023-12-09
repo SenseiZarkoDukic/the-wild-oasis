223724c3e3cb6a801c29e53aca1ef17326710b8a
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-09 21:35:30 +0100

    Add button to check in

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,21 @@ const H1 = styled.h1`
   background-color: yellow;
 `;
 
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
+
 function App() {
   return (
     <div>
       <H1>The Wild Oasis</H1>
+      <Button>Check in</Button>
     </div>
   );
 }
