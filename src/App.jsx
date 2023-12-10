dcbed1242b6d2ae2d792b70b8d22972beb695bc0
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-10 07:47:06 +0100

    Refactor Button component and import it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-500);
-  color: var(--color-brand-50);
-  box-shadow: var(--shadow-sm);
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid var(--color-gray-300);
-  background-color: var(--color-gray-0);
-  border-radius: var(--border-radius-sm);
-  box-shadow: var(--shadow-sm);
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
