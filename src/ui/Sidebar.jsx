c5aa42fdd088697cd4103eac130c168b787a057d
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-11 18:01:04 +0100

    Refactor Sidebar component styling

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: blue;
+  padding: 3.2rem 2.4rem;
+  border
+`;
+
 function Sidebar() {
-  return <aside>SIDEBAR</aside>;
+  return <StyledSidebar>SIDEBAR</StyledSidebar>;
 }
 
 export default Sidebar;
