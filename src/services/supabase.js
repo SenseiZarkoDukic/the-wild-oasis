c16a9df4de20e5b6093a269283006d177e8b07d7
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-13 01:53:33 +0100

    Update Supabase key in supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gjqflcikuoteevlkehpw.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqcWZsY2lrdW90ZWV2bGtlaHB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIzOTQzMDksImV4cCI6MjAxNzk3MDMwOX0.GBVbDPRJwd9UnCn_JoAfeHC6zdMuT1ZB6FPaT_rLkZ4";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
