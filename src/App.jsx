4d05e793644f70fad0ac1d23deb3b44c700def3b
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-13 17:44:57 +0100

    Add React Query Devtools to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Cabins from "./pages/Cabins";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,6 +27,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
