22aad89c386d3392550e48422c3c095dce01cb8a
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-13 18:18:57 +0100

    Add CabinTable component and fetch cabins data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import { useQueryClient } from "@tanstack/react-query";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
+  const cabins = useQueryClient().prefetchQuery("cabins", getCabins);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>TEST</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
