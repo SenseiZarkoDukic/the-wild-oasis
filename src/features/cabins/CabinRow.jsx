b79fb86fb7400475a135b1c864052c88465526e5
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-14 17:55:13 +0100

    Add success and error alerts when deleting a cabin

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -62,8 +62,10 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
+      alert("Cabin successfully deleted");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
     },
+    onError: (err) => alert(err.message),
   });
 
   if (isDeleting) {
