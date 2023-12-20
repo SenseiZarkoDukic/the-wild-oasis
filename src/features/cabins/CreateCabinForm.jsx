719f8a9b01e596339a228c1c615e2ff160f68d5c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-12-20 10:44:25 +0100

    Update CreateCabinForm.jsx to include file input validation

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -114,7 +114,14 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label="Cabin photo">
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          type="file"
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
