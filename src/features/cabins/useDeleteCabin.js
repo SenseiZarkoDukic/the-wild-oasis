3357e168e20037da5665a22fe2ad474fe9cdc2fd
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-06 23:27:27 +0100

    Refactor useDeleteCabin to use @tanstack/react-query

--- src/features/cabins/useDeleteCabin.js
+++ src/features/cabins/useDeleteCabin.js
@@ -1,8 +1,15 @@
-const { isLoading: isDeleting, mutate } = useMutation({
-  mutationFn: deleteCabin,
-  onSuccess: () => {
-    toast.success("Cabin successfully deleted");
-    queryClient.invalidateQueries({ queryKey: ["cabins"] });
-  },
-  onError: (err) => toast.error(err.message),
-});
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+
+export function useDeleteCabin() {
+  const queryClient = useQueryClient();
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      toast.success("Cabin successfully deleted");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+}
