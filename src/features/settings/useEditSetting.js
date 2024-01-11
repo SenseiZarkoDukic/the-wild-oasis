f49111f4ae0abefc834e17aa6150dc9e3a259275
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-11 01:47:54 +0100

    Add useUpdateSetting hook for editing settings

--- src/features/settings/useEditSetting.js
+++ src/features/settings/useEditSetting.js
@@ -1,12 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createEditCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
 
-export function useEditCabin() {
+export function useUpdateSetting() {
   const queryClient = useQueryClient();
 
-  const { isLoading: isEditing, mutate: editCabin } = useMutation({
-    mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
+  const { isLoading: isEditing, mutate: updateSetting } = useMutation({
+    mutationFn: updateSettingApi,
     onSuccess: () => {
       toast.success("Cabin successfully created");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
@@ -14,5 +15,5 @@ export function useEditCabin() {
     onError: (err) => toast.error(err.message),
   });
 
-  return { isEditing, editCabin };
+  return { isEditing, updateSetting };
 }
