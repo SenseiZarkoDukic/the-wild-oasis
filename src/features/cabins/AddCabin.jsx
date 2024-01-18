6a0859e59252c4d9311cb79fc42641400d287d7a
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-18 18:58:55 +0100

    Refactor AddCabin component to use Modal instead of form visibility state

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm";
 import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
 
 function AddCabin() {
-  const [showForm, setShowForm] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
     <div>
-      <Button onClick={() => setShowForm((show) => !show)}>
+      <Button onClick={() => setIsOpenModal((show) => !show)}>
         Add new cabin
       </Button>
-      {showForm && <CreateCabinForm />}
+      {isOpenModal && <Modal />}
     </div>
   );
 }
