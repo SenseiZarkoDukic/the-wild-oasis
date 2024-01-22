43bb9f5bbfef81f3c3004f7b27ff2eb46289dd56
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-22 16:44:40 +0100

    Remove unused imports and components in Cabins.jsx

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,6 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
-import { useQueryClient } from "@tanstack/react-query";
 import CabinTable from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import { useState } from "react";
 import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
