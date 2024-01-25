0cd7841870f3363272fbc9b377a9a203adb1aea5
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-25 12:15:07 +0100

    Update Modal component to pass onCloseModal prop to children

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -81,7 +81,7 @@ function Window({ children, name }) {
           <HiXMark />
         </Button>
 
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body
