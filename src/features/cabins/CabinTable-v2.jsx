c0cbd7fe50969c0e30e06fa5883dededca37bebb
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-26 17:38:12 +0100

    Refactor CabinTable component and introduce Table component

--- src/features/cabins/CabinTable-v2.jsx
+++ src/features/cabins/CabinTable-v2.jsx
@@ -2,7 +2,15 @@ import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
-import Table from "../../ui/Table";
+
+const Table = styled.div`
+  border: 1px solid var(--color-grey-200);
+
+  font-size: 1.4rem;
+  background-color: var(--color-grey-0);
+  border-radius: 7px;
+  overflow: hidden;
+`;
 
 const TableHeader = styled.header`
   display: grid;
