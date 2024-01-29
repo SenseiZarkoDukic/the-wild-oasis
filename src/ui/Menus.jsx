b7826330249e0977feb6dc5853bf2812b6a84d58
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-29 15:13:31 +0100

    Fix bug in login functionality

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -111,10 +111,16 @@ function List({ id, children }) {
 }
 
 function Button({ children, icon, onClick }) {
+  const { close } = useContext(MenusContext);
+  function handleClick() {
+    onClick?.();
+    close();
+  }
   return (
     <li>
-      <StyledButton onClick={onClick}>
-        {children} {icon}
+      <StyledButton onClick={handleClick}>
+        {icon}
+        <span>{children}</span>
       </StyledButton>
     </li>
   );
