e6c57fca40b7d568b6d4dbedf22d5b96c6f410b9
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-30 13:02:11 +0100

    Add CabinTableOperations component to Cabins page

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,9 @@
+import TableOperations from "../../ui/TableOperations";
+
+function CabinTableOperations() {
+  return;
+
+  <TableOperations></TableOperations>;
+}
+
+export default CabinTableOperations;
