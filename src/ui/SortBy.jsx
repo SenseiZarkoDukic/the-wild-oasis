d3ac2bc2d189495c2561bf6833870b4de6ef25de
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-02-08 16:08:33 +0100

    Refactor Select and SortBy components

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,7 +1,14 @@
+import { useSearchParams } from "react-router-dom";
 import Select from "./Select";
 
-function SortBy({ options }) {
-  function handleSortChange(e) {}
+function SortBy({ sortField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentSort = searchParams.get(sortField) || options.at(0).value;
+
+  function handleSortChange(e) {
+    searchParams.set(sortField, e.target.value);
+    setSearchParams(searchParams);
+  }
   return <Select options={options} type="white" onChange={handleSortChange} />;
 }
 
