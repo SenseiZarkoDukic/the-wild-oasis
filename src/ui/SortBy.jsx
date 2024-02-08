e7395e60974165d75f68292ac2c995d0d801920b
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-02-08 15:41:56 +0100

    Add onChange event handler to SortBy component

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,7 +1,8 @@
 import Select from "./Select";
 
 function SortBy({ options }) {
-  return <Select options={options} type="white" />;
+  function handleSortChange(e) {}
+  return <Select options={options} type="white" onChange={handleSortChange} />;
 }
 
 export default SortBy;
