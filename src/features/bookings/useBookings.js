cf1874b9de09f96d77a7f2270cad994f1f31b3dc
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-02-11 22:15:48 +0100

    Add search parameter to useBookings hook

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
   const {
     isLoading,
     data: bookings,
