292c4163ee818c337eab1af42ca0decdbaf3b2de
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-05-20 16:52:10 +0200

    chore: Refactor FormRowVertical component to use flexbox for layout

--- src/ui/FormRowVertical.jsx
+++ src/ui/FormRowVertical.jsx
@@ -1,30 +1,10 @@
 import styled from "styled-components";
 
-const StyledFormRowVertical = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
+const StyledFormRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
   padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
 `;
 
 const Label = styled.label`
@@ -38,11 +18,11 @@ const Error = styled.span`
 
 function FormRowVertical({ label, error, children }) {
   return (
-    <StyledFormRowVertical>
+    <StyledFormRow>
       {label && <Label htmlFor={children.props.id}>{label}</Label>}
       {children}
       {error && <Error>{error}</Error>}
-    </StyledFormRowVertical>
+    </StyledFormRow>
   );
 }
 
