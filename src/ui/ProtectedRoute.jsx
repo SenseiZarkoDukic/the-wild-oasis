b4745807ea6798b860fce7e4dd8b788bff655c59
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-05-22 18:51:05 +0200

    chore: Add getCurrentUser function to apiAuth.js and implement ProtectedRoute component in ProtectedRoute.jsx

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,4 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function ProtectedRoute({ children }) {
+  // 1. Load the authenticated user
+
+  // 2. While loading, show spinner
+
+  // 3. If user is not authenticated, redirect to login
+
+  // 4. If there IS a user, render the app
+
   return children;
 }
 
