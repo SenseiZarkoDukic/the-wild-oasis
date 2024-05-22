1753eb856c37484656c7bd7ab09bc5345b8d96b3
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-05-22 17:51:49 +0200

    chore: Update useLogin hook to handle login form submission

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,8 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
-  useMutation({
+  const navigate = useNavigate();
+  const { mutate, isLoading } = useMutation({
     mutationFn: ({ email, password }) => login({ email, password }),
+    onSuccess: () => {
+      // Redirect to the dashboard
+      navigate("/dashboard");
+    },
   });
 }
