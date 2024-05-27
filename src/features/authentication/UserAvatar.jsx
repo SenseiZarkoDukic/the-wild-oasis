86d17dea890090540753ba33e53f88dca8b4407e
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-05-27 18:49:10 +0200

    feat: Add useUser hook to UserAvatar component
    This commit adds the useUser hook to the UserAvatar component, allowing it to access user data such as full name and avatar. The hook is imported from the useUser module and used to retrieve the user object from the user context. This enhancement improves the UserAvatar component by dynamically displaying the user's full name and avatar in the header.

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useUser } from "./useUser";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -19,3 +20,23 @@ const Avatar = styled.img`
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+function UserAvatar() {
+  const { user } = useUser();
+  // console.log(user);
+  const { fullName, avatar } = user.user_metadata;
+
+  console.log(fullName, avatar);
+
+  return (
+    <StyledUserAvatar>
+      <Avatar
+        src="https://randomuser.me/api/port...0&noinfo"
+        alt="User avatar"
+      />
+      <span>John Doe</span>
+    </StyledUserAvatar>
+  );
+}
+
+export default UserAvatar;
