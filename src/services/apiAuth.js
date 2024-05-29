79fde6a23a2970f59a5313691b279026d525865c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-05-29 17:24:12 +0200

    feat: Update updateCurrentUser function in apiAuth service
    This commit updates the updateCurrentUser function in the apiAuth service to handle updating the user's password and full name separately. The function now checks if the password or full name is provided and updates the corresponding data accordingly. This improvement enhances the functionality of the updateCurrentUser function, allowing users to update their password or full name individually.

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -48,8 +48,18 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
   // 1. Update password OR fullName
   let updateData;
   if (password) updateData = { password };
-  if (fullName) updateData = { fullName };
-  const { data, error } = await supabase.auth.updateUser({});
+  if (fullName) updateData = { data: { fullName } };
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
   // 2. Upload the avatar image
-  // 3. Update avatar in he user
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // 3. Update avatar in the user
 }
