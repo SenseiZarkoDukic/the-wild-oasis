0b9a689fcdf88125f82b330a26625976fe6aef80
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-06-03 17:10:27 +0200

    feat: Add numDays parameter to useRecentBookings hook

--- src/features/dashboard/useRecentBookings.js
+++ src/features/dashboard/useRecentBookings.js
@@ -2,4 +2,8 @@ import { useSearchParams } from "react-router-dom";
 
 export function useRecentBookings() {
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
 }
