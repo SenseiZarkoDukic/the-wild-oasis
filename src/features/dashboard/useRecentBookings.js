3ebdfe9d851e665dd7df0d9ee4a30f524336b1c1
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-06-03 17:18:17 +0200

    feat: Add lazy loading for images in useRecentBookings hook
    This commit adds lazy loading for images in the useRecentBookings hook. It imports the useQuery hook from the @tanstack/react-query package and the subDays function from the date-fns package. It also imports the getBookingsAfterDate function from the ../../services/apiBookings module. The hook now fetches and displays recent bookings after a specified number of days, improving the performance of the Dashboard page by loading images only when necessary.

--- src/features/dashboard/useRecentBookings.js
+++ src/features/dashboard/useRecentBookings.js
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
 import { useSearchParams } from "react-router-dom";
+import { getBookingsAfterDate } from "../../services/apiBookings";
 
 export function useRecentBookings() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -6,4 +9,11 @@ export function useRecentBookings() {
   const numDays = !searchParams.get("last")
     ? 7
     : Number(searchParams.get("last"));
+
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isPending, data: bookings } = useQuery({
+    queryFn: () => getBookingsAfterDate(queryDate),
+    queryKey: () => ["bookings", `last-${numDays}`],
+  });
 }
