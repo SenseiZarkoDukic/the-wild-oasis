2aef841c5b2ed14eea60f2376f1a58487fee4dbd
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-06-08 22:42:45 +0200

    refactor: Update useTodayActivity hook to use react-query
    This commit updates the useTodayActivity hook to utilize the react-query library for handling API queries. By integrating react-query, the hook can now efficiently fetch today's activity data from the server and manage caching, background updates, and error handling. This enhancement improves the performance and reliability of the check-in/out feature.

--- src/features/check-in-out/useTodayActivity.js
+++ src/features/check-in-out/useTodayActivity.js
@@ -1,1 +1,9 @@
-export function useTodayActivity() {}
+import { useQuery } from "@tanstack/react-query";
+import { getStaysTodayActivity } from "../../services/apiBookings";
+
+export function useTodayActivity() {
+  useQuery({
+    queryFn: getStaysTodayActivity,
+    queryKey: ["today-activity"],
+  });
+}
